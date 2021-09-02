3b1e8105bf40c104297a720a78ab41cef2975a35
mouludin <muhammadmauludin.1188@gmail.com>
2021-09-02 18:50:14 +0700

    firs

---
+++ src/settings.4d836.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/Game.fire",orientation:"landscape",jsList:[],bundleVers:{internal:"279e0",main:"ad01a"}};
